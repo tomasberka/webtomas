539c834356151ca9b15dc05717368506b04408d9
tommymyhony <tomasberka99@seznam.cz>
2026-01-03 02:32:13 +0100

    feat: Add London landing page with SEO optimizations

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -15,6 +15,7 @@ export default function sitemap(): MetadataRoute.Sitemap {
         '/partner-socialvids',
         '/kontakt',
         '/blog',
+        '/london',
     ].map((route) => ({
         url: `${baseUrl}${route}`,
         lastModified: new Date(),
